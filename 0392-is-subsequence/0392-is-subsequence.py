22596fa6d4f731e0c2bacf498091ab78ff9a58fb
Vikram Choudhary <32350697+vikramiiitm@users.noreply.github.com>
2023-09-22 09:38:37 +0530

    Time: 50 ms (8.8%), Space: 16.3 MB (84.5%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,4 +1,6 @@
 class Solution:
+    # We are looping through t for every element of s, we keep slicing(keep the right side) the t until the char is not found in the string2, if found add the char in parsed string
+    #compare the parsed string after loop if it equals to s
     def isSubsequence(self, s: str, t: str) -> bool:
         parsed = ''
         for i in s:
